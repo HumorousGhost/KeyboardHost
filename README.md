f4ae6ca81b7581cc2eafda5ce0655cac7814495f
v-zhangliqun <v-zhangliqun@360.cn>
2022-07-26 17:58:53 +0800

    1.0.0

--- README.md
+++ README.md
@@ -1,3 +1,27 @@
 # KeyboardHost
 
-A description of this package.
+This project is only for SwiftUI
+
+When View uses `ignoresSafeArea` or `edgesIgnoringSafeArea(.bottom)`
+
+this framework can be used because `TextField` and `TextEditor` cannot make the keyboard bounce
+
+Encapsulated `UITextView`, `UITextField` can also be used
+
+### Demo
+
+```swift
+    @State private var text: String = ""
+    
+    var body: some View {
+        VStack {
+            Spacer()
+            KeyboardHost {
+                TextField("please enter text", text: $text)
+                    .frame(width: 200, height: 40, alignment: .center)
+                    .padding(.bottom, 20)
+            }
+        }
+        .ignoresSafeArea()
+    }
+```
--- Sources/KeyboardHost/KeyboardHost.swift
+++ Sources/KeyboardHost/KeyboardHost.swift
@@ -1,6 +1,45 @@
-public struct KeyboardHost {
-    public private(set) var text = "Hello, World!"
+import SwiftUI
 
-    public init() {
+@available(iOS 13.0, macOS 10.15, tvOS 13.0, watchOS 6.0, *)
+public struct KeyboardHost<Content>: View where Content: View {
+    
+    /// init
+    /// - Parameter content: some View
+    public init (@ViewBuilder content: () -> Content) {
+        view = content()
+    }
+    
+    public var body: some View {
+        VStack {
+            view
+            Spacer()
+                .frame(height: keyboardHeight)
+                .animation(.default)
+                .foregroundColor(.clear)
+        }.onReceive(showPublisher.merge(with: hidePublisher)) { (height) in
+            self.keyboardHeight = height
+        }
+    }
+    
+    let view: Content
+    
+    @State private var keyboardHeight: CGFloat = 0
+    
+    private let showPublisher = NotificationCenter.Publisher.init(
+        center: .default,
+        name: UIResponder.keyboardWillShowNotification
+    ).map { (notification) -> CGFloat in
+        if let rect = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? CGRect {
+            return rect.size.height
+        } else {
+            return 0
+        }
+    }
+    
+    private let hidePublisher = NotificationCenter.Publisher.init(
+        center: .default,
+        name: UIResponder.keyboardWillHideNotification
+    ).map { _ -> CGFloat in
+        return 0
     }
 }
